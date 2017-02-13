6d33e9085401829167794e2477382d3dac7b41ff
Christina L. Cole <christinalcole@gmail.com>
2017-02-13 00:07:45 -0500

    initial new book create route, actions, view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+
+  get '/books/new' do
+    erb :'books/new'
+  end
+
+  post '/books' do
+    @book = Book.create(params[:book])
+    current_user.books << @book
+    redirect to "/books/#{@book.slug}"
+  end
+  
 end
+
+
+# {"book"=>{"title"=>"Dwight's Book", "author"=>"Dwight's Author"},
+#  "submit"=>"Add Book"}
