a2192f1dcfa57ada9feefdca664353fc26487639
Christina L. Cole <christinalcole@gmail.com>
2017-02-14 13:05:09 -0500

    revised schema, goes with commit fd500d1986fc393479cef2980af0493491622295

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210185111) do
+ActiveRecord::Schema.define(version: 20170213223300) do
 
   create_table "artworks", force: :cascade do |t|
     t.string "name"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20170210185111) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+    t.string  "title"
+    t.string  "author"
+    t.integer "creator_id"
   end
 
   create_table "songs", force: :cascade do |t|
