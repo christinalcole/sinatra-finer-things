df133a2e350448b607d696efc906914120100903
Christina L. Cole <christinalcole@gmail.com>
2017-02-17 23:48:49 -0500

    adding creator to song model, creator_id to songs table

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,8 @@ class Song < ActiveRecord::Base
   has_many :user_songs
   has_many :users, through: :user_songs
 
+  belongs_to :creator, :class_name => "User"
+
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
 end
