d1b0de7272e28b84be8514d8c9c19db3da5a8e6b
Christina L. Cole <christinalcole@gmail.com>
2017-02-18 00:00:45 -0500

    adding song index view, action, route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,10 @@
 class SongsController < ApplicationController
+  get '/songs' do
+    if is_logged_in?
+      @songs = Song.all
+      erb :'songs/index'
+    else
+      redirect to "/login"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216035213) do
+ActiveRecord::Schema.define(version: 20170218043421) do
 
   create_table "artworks", force: :cascade do |t|
     t.string  "name"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20170216035213) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string "name"
-    t.string "artist"
+    t.string  "name"
+    t.string  "artist"
+    t.integer "creator_id"
   end
 
   create_table "user_artworks", force: :cascade do |t|
