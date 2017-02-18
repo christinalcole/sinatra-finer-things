599788b3a718025e7f8213d3acf64b876e089a62
Christina L. Cole <christinalcole@gmail.com>
2017-02-18 00:44:17 -0500

    completing song object edit action, routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,7 +32,12 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
     if is_logged_in?
       @song = Song.find_by_slug(params[:slug])
-      erb :'songs/edit'
+      if @song.creator_id == current_user.id
+        erb :'songs/edit'
+      else
+        flash[:message] = "Sorry, you can only edit music that you previously entered into the database"
+        redirect to "/songs/#{@song.slug}"
+      end
     else
       redirect to "/login"
     end
@@ -44,6 +49,18 @@ class SongsController < ApplicationController
       erb :'songs/show'
     else
       redirect to "/login"
+    end
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    if @song.valid?
+      flash[:message] = "This music has been successfully updated"
+      redirect to "/songs/#{@song.slug}"
+    else
+      flash[:message] = @song.errors.full_messages
+      redirect to "/songs/#{Song.find_by_slug(params[:slug]).slug}/edit"
     end
   end
 end
