a671e8b64ba6ba00ce8ff4203ef336dfcdf91806
Christina L. Cole <christinalcole@gmail.com>
2017-02-18 00:19:59 -0500

    completion of song object create action, routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,14 @@ class SongsController < ApplicationController
     end
   end
 
+  post '/songs' do
+    @song = Song.find_or_create_by(params[:song]) do |s|
+      s.creator_id = current_user.id
+    end
+    current_user.songs << @song
+    redirect to "/users/#{current_user.slug}"
+  end
+
   get '/songs/:slug' do
     if is_logged_in?
       @song = Song.find_by_slug(params[:slug])
